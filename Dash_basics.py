d13a81e5c333cce49fdd3013b085b265f9738962
justcro <48687630+justcro@users.noreply.github.com>
2023-12-19 14:33:37 -0500

    Create Dash_basics.py
    import: command not found

---
+++ Dash_basics.py
@@ -0,0 +1,28 @@
+###python3 -m pip install packaging
+###python3 -m pip install pandas dash
+###python3 -m pip install pandas dash
+###These packages were installed at startup
+
+theia@theiadocker-justcro:/home/project$ # Import required packages
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+
+# Read the airline data into pandas dataframe
+iles/airline_data.csv', csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files                            encoding = "ISO-8859-1",
+s')oup', title='Distance group proportion by flighttr, 'Div1TailNum': str, 
+bash: import: command not foundpe={'Div1Airport': sbash: import: command not foundproportion by flight
+bash: import: command not found
+bash: from: command not found
+bash: from: command not found
+bash: syntax error near unexpected token `('
+bash: encoding: command not found
+bash: str,: command not found
+bash: syntax error near unexpected token `)'
+bash: syntax error near unexpected token `('
+bash: syntax error near unexpected token `('
+theia@theiadocker-justcro:/home/project$ 
+
+
